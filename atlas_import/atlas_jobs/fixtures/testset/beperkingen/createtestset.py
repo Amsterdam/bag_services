74a097e02c59104eb02c66777d017e7d3fb4d093
Raymond Nijssen <r.nijssen@terglobo.nl>
2015-08-05 10:30:32 +0200

    testdata beperkingen

---
+++ atlas_import/atlas_jobs/fixtures/testset/beperkingen/createtestset.py
@@ -0,0 +1,42 @@
+infn = 'wpb_belemmering_50.dat'
+
+def createBelPerc(ids):
+    basefn = 'wpb_belemmering_perceel'
+    infile = open(basefn + '.dat','r')
+    outfile = open(basefn + '_50.dat','w')
+    for line in infile:
+        items = line.strip().split(';')
+        if items[5] in ids:
+            outfile.write(line)
+    infile.close()
+    outfile.close()
+
+def createBrondoc(nrs):
+    basefn = 'wpb_brondocument'
+    infile = open(basefn + '.dat','r')
+    outfile = open(basefn + '_50.dat','w')
+    for line in infile:
+        items = line.strip().split(';')
+        if items[0] in nrs:
+            outfile.write(line)
+    infile.close()
+    outfile.close()
+
+
+infile = open(infn,'r')
+ids = []
+nrs = []
+for line in infile:
+    #print line
+    items = line.strip().split(';')
+    #print items[0], items[1]
+    ids.append(items[0])
+    nrs.append(items[1])
+infile.close()
+
+print len(ids), len(nrs)
+print ids
+print nrs
+createBelPerc(ids)
+createBrondoc(nrs)    
+    
