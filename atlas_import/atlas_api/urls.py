ce645779abc36cca59521345f0553b3f98441ca5
Yigal Duppen <yigal@publysher.nl>
2015-09-08 10:17:11 +0200

    ATLAS-39 Change URLs to reflect owning dataset

--- atlas_import/atlas_api/urls.py
+++ atlas_import/atlas_api/urls.py
@@ -3,13 +3,22 @@ from rest_framework import routers
 from atlas_api import views
 import bag.views
 
-router = routers.DefaultRouter()
-router.register(r'ligplaatsen', bag.views.LigplaatsViewSet)
-router.register(r'standplaatsen', bag.views.StandplaatsViewSet)
-router.register(r'verblijfsobjecten', bag.views.VerblijfsobjectViewSet)
-router.register(r'nummeraanduidingen', bag.views.NummeraanduidingViewSet)
-router.register(r'panden', bag.views.PandViewSet)
-router.register(r'typeahead', views.TypeaheadViewSet, base_name='typeahead')
+class DocumentedRouter(routers.DefaultRouter):
+    def get_api_root_view(self):
+        view = super().get_api_root_view()
+        print(view)
+        view.description = "Hello world"
+        return view
+
+
+router = DocumentedRouter()
+router.register(r'bag/ligplaatsen', bag.views.LigplaatsViewSet)
+router.register(r'bag/standplaatsen', bag.views.StandplaatsViewSet)
+router.register(r'bag/verblijfsobjecten', bag.views.VerblijfsobjectViewSet)
+router.register(r'bag/nummeraanduidingen', bag.views.NummeraanduidingViewSet)
+router.register(r'bag/panden', bag.views.PandViewSet)
+
+router.register(r'atlas/typeahead', views.TypeaheadViewSet, base_name='typeahead')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
