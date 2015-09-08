f263ba230b828d3979ff72fe167abd9a3e9965e1
Yigal Duppen <yigal@publysher.nl>
2015-09-08 11:17:37 +0200

    ATLAS-39 Add top-level documentation

--- atlas_import/atlas_api/urls.py
+++ atlas_import/atlas_api/urls.py
@@ -4,9 +4,25 @@ from atlas_api import views
 import bag.views
 
 class DocumentedRouter(routers.DefaultRouter):
+    """
+    Onderstaande lijst bevat alle API's die door Datapunt zelf worden geleverd.
+
+    `bag/`
+    :   [Basisadministratie Adressen en Gebouwen](http://www.amsterdam.nl/stelselpedia/bag-index/)
+
+    `atlas/`
+    :   Specifieke functionaliteit voor Atlas
+    """
+
     def get_api_root_view(self):
         view = super().get_api_root_view()
-        return view
+        cls = view.cls
+
+        class Datapunt(cls):
+            pass
+
+        Datapunt.__doc__ = self.__doc__
+        return Datapunt.as_view()
 
 
 router = DocumentedRouter()
