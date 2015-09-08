e5807b0e2e19a61d9b0263918ba5c6bb59c9250b
Yigal Duppen <yigal@publysher.nl>
2015-09-08 14:17:12 +0200

    ATLAS-39 Add WKPB admin

--- atlas_import/datasets/wkpb/admin.py
+++ atlas_import/datasets/wkpb/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class ReadOnlyAdmin(admin.ModelAdmin):
+    readonly_fields = []
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + [field.name for field in obj._meta.fields]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class CodeOmschrijvingAdmin(ReadOnlyAdmin):
+    list_display = ('code', 'omschrijving')
+
+class BeperkingAdmin(ReadOnlyAdmin):
+    list_display = ('inschrijfnummer', 'beperkingtype')
+
+
+admin.site.register(models.Beperkingcode, CodeOmschrijvingAdmin)
+admin.site.register(models.Broncode, CodeOmschrijvingAdmin)
+admin.site.register(models.Beperking, BeperkingAdmin)
