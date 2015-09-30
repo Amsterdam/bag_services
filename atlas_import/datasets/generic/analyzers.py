0d3ef0de13418a0124b3c08e228feb4931fe4443
Yigal Duppen <yigal@publysher.nl>
2015-09-30 16:54:35 +0200

    ATLAS-113 Synonym filter, change tokenization

--- atlas_import/datasets/generic/analyzers.py
+++ atlas_import/datasets/generic/analyzers.py
@@ -1,9 +1,26 @@
 import elasticsearch_dsl as es
+from elasticsearch_dsl import analysis
+
+synonym_filter = analysis.token_filter('synonyms', type='synonym', synonyms=[
+    '1e=>eerste',
+    '2e=>tweede',
+    '3e=>derde',
+    '4e=>vierde',
+])
 
 kadastrale_aanduiding = es.analyzer('kadastrale_aanduiding', tokenizer='keyword', filter=['standard', 'lowercase'])
-adres = es.analyzer('adres', tokenizer='keyword', filter=['standard', 'lowercase', 'asciifolding'])
 
-naam = es.analyzer('naam', tokenizer='keyword', filter=['standard', 'lowercase', 'asciifolding'])
+adres = es.analyzer('adres',
+                    tokenizer='standard',
+                    filter=['standard', 'lowercase', 'asciifolding', synonym_filter],
+                    )
 
-autocomplete = es.analyzer('completions', tokenizer='keyword',
-                           filter=['standard', 'lowercase', 'asciifolding'])
+naam = es.analyzer('naam',
+                   tokenizer='standard',
+                   filter=['standard', 'lowercase', 'asciifolding', synonym_filter],
+                   )
+
+autocomplete = es.analyzer('completions',
+                           tokenizer='standard',
+                           filter=['standard', 'lowercase', 'asciifolding', synonym_filter],
+                           )
