34d847af0dc00441c1e1b3c791dca6fd1295c96e
Yigal Duppen <yigal@publysher.nl>
2015-10-29 13:58:27 +0100

    Dockerize project

---
+++ atlas_import/atlas_import/settings/docker.py
@@ -0,0 +1,48 @@
+import re
+from .settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'insecure',
+        'HOST': os.getenv('DATABASE_PORT_5432_TCP_ADDR'),
+        'PORT': os.getenv('DATABASE_PORT_5432_TCP_PORT'),
+        'CONN_MAX_AGE': 60,
+    }
+}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console'],
+    },
+    'loggers': {
+    },
+}
+
+ELASTIC_SEARCH_HOSTS = ["{}:{}".format(os.getenv('ELASTICSEARCH_PORT_9200_TCP_ADDR'),
+                                       os.getenv('ELASTICSEARCH_PORT_9200_TCP_PORT'))]
+
+PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', '..'))
+DIVA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'atlas_import', 'diva'))
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DEBUG = os.getenv('DJANGO_DEBUG', False)
