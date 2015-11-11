5a4d2d82fb3d2d4e1fa027e96ce1e08335e8fefa
Yigal Duppen <yigal@publysher.nl>
2015-11-11 11:45:04 +0100

    Better logging on failure

--- atlas_import/health/views.py
+++ atlas_import/health/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 from django.db import connection
 from django.http import HttpResponse
@@ -7,6 +9,9 @@ from atlas_api.views import get_autocomplete_response
 from datasets.bag.models import Verblijfsobject
 
 
+log = logging.getLogger(__name__)
+
+
 def health(request):
     # check database
     try:
@@ -14,6 +19,7 @@ def health(request):
             cursor.execute("select 1")
             assert cursor.fetchone()
     except:
+        log.exception()
         return HttpResponse("Database connectivity failed", content_type="text/plain", status=500)
 
     # check elasticsearch
@@ -21,6 +27,7 @@ def health(request):
         client = Elasticsearch(settings.ELASTIC_SEARCH_HOSTS)
         assert client.info()
     except:
+        log.exception()
         return HttpResponse("Elasticsearch connectivity failed", content_type="text/plain", status=500)
 
     return HttpResponse("Connectivity OK", content_type='text/plain', status=200)
@@ -31,6 +38,7 @@ def check_data(request):
     try:
         assert Verblijfsobject.objects.count() > 10
     except:
+        log.exception()
         return HttpResponse("No BAG data found", content_type="text/plain", status=500)
 
     # check elastic
@@ -38,6 +46,7 @@ def check_data(request):
         client = Elasticsearch(settings.ELASTIC_SEARCH_HOSTS)
         assert get_autocomplete_response(client, 'weesp')
     except:
+        log.exception()
         return HttpResponse("Autocomplete failed", content_type="text/plain", status=500)
 
-    return HttpResponse("OK", content_type='text/plain', status=200)
+    return HttpResponse("Data OK", content_type='text/plain', status=200)
