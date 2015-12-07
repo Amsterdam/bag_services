43a7d432605980e7498e396bb96512cd710a36b5
Yigal Duppen <yigal@publysher.nl>
2015-12-07 15:13:20 +0100

    ATLAS-337 Import user script

---
+++ atlas_import/atlas_api/management/commands/import_users.py
@@ -0,0 +1,32 @@
+import csv
+
+import datetime
+from django.contrib.auth.models import User
+from django.core.management import BaseCommand
+from django.utils.timezone import UTC
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument("csv_file")
+
+    def handle(self, *args, **options):
+        csv_file = options['csv_file']
+
+        with open(csv_file) as f:
+            reader = csv.reader(f, delimiter=';')
+            next(reader) # skip header
+
+            for row in reader:
+                username = row[0].lower()
+                password = row[1]
+                email = row[2]
+                joined = datetime.datetime.strptime(row[3], "%d-%m-%Y")
+                joined = joined.replace(tzinfo=UTC())
+
+                User.objects.filter(username=username).delete()
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.date_joined = joined
+                user.save()
+
