2b21630c724ae91fcde4933af83918cd68dd3f29
Richard Jansen <richard.jansen@lajos.nl>
2015-12-15 13:34:07 +0100

    skip unfinished tests (please?)

--- atlas_import/datasets/brk/tests/test_sensitive_details.py
+++ atlas_import/datasets/brk/tests/test_sensitive_details.py
@@ -6,7 +6,6 @@ from rest_framework.test import APITestCase
 from datasets.akr.tests import factories
 
 
-@skip
 class SensitiveDetailsTestCase(APITestCase):
     def setUp(self):
         self.not_authorized = User.objects.create_user(username='not_authorized', password='pass')
@@ -30,12 +29,14 @@ class SensitiveDetailsTestCase(APITestCase):
             transactie=self.transactie,
         )
 
+    @skip
     def test_niet_ingelogd_geen_details_in_natuurlijk_persoon_json(self):
         response = self.client.get('/api/brk/subject/{}.json'.format(self.natuurlijk.pk)).data
         self.assertNotIn('rechten', response)
         self.assertNotIn('woonadres', response)
         self.assertNotIn('postadres', response)
 
+    @skip
     def test_niet_ingelogd_wel_details_in_niet_natuurlijk_persoon_json(self):
         response = self.client.get('/api/brk/subject/{}.json'.format(self.niet_natuurlijk.pk)).data
 
@@ -43,6 +44,7 @@ class SensitiveDetailsTestCase(APITestCase):
         self.assertIsNotNone(response['woonadres'])
         self.assertIsNotNone(response['postadres'])
 
+    @skip
     def test_ingelogd_niet_geautoriseerd_geen_details_in_natuurlijk_json(self):
         self.client.login(username='not_authorized', password='pass')
         response = self.client.get('/api/brk/subject/{}.json'.format(self.natuurlijk.pk)).data
@@ -51,6 +53,7 @@ class SensitiveDetailsTestCase(APITestCase):
         self.assertNotIn('woonadres', response)
         self.assertNotIn('postadres', response)
 
+    @skip
     def test_ingelogd_wel_geautoriseed_wel_details_in_natuurlijk_persoon_json(self):
         self.client.login(username='authorized', password='pass')
         response = self.client.get('/api/brk/subject/{}.json'.format(self.natuurlijk.pk)).data
@@ -59,6 +62,7 @@ class SensitiveDetailsTestCase(APITestCase):
         self.assertIsNotNone(response['woonadres'])
         self.assertIsNotNone(response['postadres'])
 
+    @skip
     def test_ingelogd_zakelijk_recht_verwijst_naar_hoofd_view(self):
         self.client.login(username='authorized', password='pass')
         response = self.client.get('/api/brk/zakelijk-recht/{}.json'.format(self.natuurlijk_recht.pk)).data
@@ -66,18 +70,21 @@ class SensitiveDetailsTestCase(APITestCase):
         subj = response['kadastraal_subject']
         self.assertEqual(subj['_links']['self']['href'], 'http://testserver/api/brk/subject/{}/'.format(self.natuurlijk.pk))
 
+    @skip
     def test_uitgelogd_zakelijk_recht_niet_natuurlijk_verwijst_naar_hoofd_view(self):
         response = self.client.get('/api/brk/zakelijk-recht/{}.json'.format(self.niet_natuurlijk_recht.pk)).data
 
         subj = response['kadastraal_subject']
         self.assertEqual(subj['_links']['self']['href'], 'http://testserver/api/brk/subject/{}/'.format(self.niet_natuurlijk.pk))
 
+    @skip
     def test_uitgelogd_zakelijk_recht_natuurlijk_verwijst_naar_subresource(self):
         response = self.client.get('/api/brk/zakelijk-recht/{}/'.format(self.natuurlijk_recht.pk)).data
 
         subj = response['kadastraal_subject']
         self.assertEqual(subj, 'http://testserver/api/brk/zakelijk-recht/{}/subject/'.format(self.natuurlijk_recht.pk))
 
+    @skip
     def test_subresource_toon_persoonsgegevens_maar_geen_relaties(self):
         response = self.client.get('/api/brk/zakelijk-recht/{}/subject.json'.format(self.natuurlijk_recht.pk)).data
         self.assertIsNotNone(response['woonadres'])
