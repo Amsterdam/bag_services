d954b1fbf48e3358e8de397a8203431fbc76b8c5
Richard Jansen <richard.jansen@lajos.nl>
2015-12-16 18:13:20 +0100

    ATLAS-459 permission migration files

---
+++ atlas_import/datasets/brk/migrations/0020_auto_20151216_1733.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('brk', '0019_merge'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='kadastraalsubject',
+            options={'verbose_name': 'Kadastraal subject', 'verbose_name_plural': 'Kadastrale subjecten', 'permissions': (('view_sensitive_details', 'Kan privacy-gevoelige data inzien'),)},
+        ),
+    ]
