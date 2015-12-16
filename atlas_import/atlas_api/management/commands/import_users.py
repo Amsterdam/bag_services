961e45f66db328344ef0619474998f7564736b61
Richard Jansen <richard.jansen@lajos.nl>
2015-12-16 17:58:09 +0100

    ATLAS-478 correct permissions for new users

--- atlas_import/atlas_api/management/commands/import_users.py
+++ atlas_import/atlas_api/management/commands/import_users.py
@@ -2,8 +2,12 @@ import csv
 
 import datetime
 from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
 from django.utils.timezone import UTC
+
+from datasets.akr import models as akr_models
+from datasets.brk import models as brk_models
 
 
 class Command(BaseCommand):
@@ -14,7 +18,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         csv_file = options['csv_file']
 
-        view_sensitive_details_permission = Permission.objects.get(codename='view_sensitive_details')
+        view_sensitive_details_permission_akr = Permission.objects.get(
+            content_type=ContentType.objects.get_for_model(akr_models.KadastraalSubject),
+            codename='view_sensitive_details'
+        )
+
+        view_sensitive_details_permission_brk = Permission.objects.get(
+            content_type=ContentType.objects.get_for_model(brk_models.KadastraalSubject),
+            codename='view_sensitive_details'
+        )
 
         with open(csv_file) as f:
             reader = csv.reader(f, delimiter=';')
@@ -30,5 +42,6 @@ class Command(BaseCommand):
                 User.objects.filter(username=username).delete()
                 user = User.objects.create_user(username=username, password=password, email=email)
                 user.date_joined = joined
-                user.user_permissions.add(view_sensitive_details_permission)
+                user.user_permissions.add(view_sensitive_details_permission_akr)
+                user.user_permissions.add(view_sensitive_details_permission_brk)
                 user.save()
