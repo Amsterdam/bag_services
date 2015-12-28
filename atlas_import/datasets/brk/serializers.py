64485048abcb8fdec81f9d8f1a1ff69ccf719ae7
Richard Jansen <richard.jansen@lajos.nl>
2015-12-28 17:03:17 +0100

    ATLAS-468 revert last commit

--- atlas_import/datasets/brk/serializers.py
+++ atlas_import/datasets/brk/serializers.py
@@ -278,7 +278,6 @@ class KadastraalSubjectDetailWithPersonalData(BrkMixin, rest.HALSerializer):
 
 class KadastraalSubjectDetail(KadastraalSubjectDetailWithPersonalData):
     rechten = rest.RelatedSummaryField()
-    kadastrale_objecten = rest.RelatedSummaryField()
 
     allowed_anonymous = {'_links', '_display', 'id', 'volledige_naam', 'is_natuurlijk_persoon'}
 
@@ -322,7 +321,6 @@ class KadastraalSubjectDetail(KadastraalSubjectDetailWithPersonalData):
 
             'aantekeningen',
             'rechten',
-            'kadastrale_objecten',
         )
 
     def to_representation(self, instance):
