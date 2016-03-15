4654eda0a27417e87154ad0fe56617912ef6e0fb
Kfir Breger <kfir@cubicalwave.com>
2016-03-15 14:41:19 +0100

    Adding aggregation to kadaster queries

--- atlas_import/datasets/brk/queries.py
+++ atlas_import/datasets/brk/queries.py
@@ -13,14 +13,14 @@ from elasticsearch_dsl import Search, Q, A
 def kadaster_object_Q(query):
     """Create query/aggregation for kadaster object search"""
     return {
-        'A': None,
+        'A': A('terms', field='aanduiding.raw'),
         'Q': Q('match_phrase_prefix', aanduiding=query)
     }
 
 def kadaster_subject_Q(query):
     """Create query/aggregation for kadaster subject search"""
     return {
-        'A': None,
+        'A': A('terms', field='naam.raw'),
         'Q': Q(
             'multi_match',
             query=query,
