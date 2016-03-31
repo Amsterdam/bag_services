eced43d02b9f4e4beffc36701d186af044082e42
Richard Jansen <richard.jansen@lajos.nl>
2016-03-31 13:42:26 +0200

    ATLAS-1365 urls for acc and prod + tests

--- atlas_import/datasets/generic/metadata.py
+++ atlas_import/datasets/generic/metadata.py
@@ -1,24 +1,38 @@
 import datetime
 import requests
+import socket
 
 from datasets.generic import uva2
 
 
 class UpdateDatasetMixin(object):
     dataset_id = None
+    import_hostname = None
+
+    def set_hostname(self, hostname=None):
+        if hostname:
+            self.import_hostname = hostname
+        else:
+            self.import_hostname = socket.gethostname()
+
+    @property
+    def uri(self):
+        if '-acc' in self.import_hostname:
+            return 'https://api-acc.datapunt.amsterdam.nl/metadata/'
+
+        return 'https://api.datapunt.amsterdam.nl/metadata/'
 
     def update_metadata_uva2(self, path, code):
         filedate = uva2.get_uva2_filedate(path, code)
 
-        self.update_metadata_date(filedate)
+        return self.update_metadata_date(filedate)
 
     def update_metadata_onedate(self, path, code):
         filedate = uva2.get_filedate(path, code)
 
-        self.update_metadata_date(filedate)
+        return self.update_metadata_date(filedate)
 
     def update_metadata_date(self, date):
-        return
         if not date:
             return
 
@@ -28,4 +42,9 @@ class UpdateDatasetMixin(object):
             'last_import_date': datetime.date.today(),
         }
 
-        requests.put('/metadata/api/{}/'.format(self.dataset_id), data)
+        self.set_hostname()
+        uri = '%s%s/' % (self.uri, self.dataset_id)
+
+        res = requests.put(uri, data)
+
+        return res
---
+++ atlas_import/datasets/generic/tests/test_metadata.py
@@ -0,0 +1,54 @@
+import os
+
+from django.conf import settings
+from django.test import TestCase
+
+from datasets.generic import metadata
+
+
+class MetadataTest(TestCase, metadata.UpdateDatasetMixin):
+    diva = None
+
+    def setUp(self):
+        self.diva = settings.DIVA_DIR
+        if not os.path.exists(self.diva):
+            raise ValueError("DIVA_DIR not found: {}".format(self.diva))
+
+        self.set_hostname()
+
+        if '-acc' in self.import_hostname:
+            self.dataset_id = 'test-acc'
+        else:
+            self.dataset_id = 'test-prod'
+
+    def testUriAcc(self):
+        self.set_hostname('ap01-acc.datapunt.amsterdam.nl')
+
+        self.assertEqual(self.uri, 'https://api-acc.datapunt.amsterdam.nl/metadata/')
+
+    def testUriProd(self):
+        self.set_hostname('ap01.datapunt.amsterdam.nl')
+
+        self.assertEqual(self.uri, 'https://api.datapunt.amsterdam.nl/metadata/')
+
+    def testNoDate(self):
+        response = self.update_metadata_date(None)
+        self.assertEqual(response, None)
+
+    def testOneDateAcc(self):
+        self.path = os.path.join(self.diva, 'brk')
+        self.set_hostname('ap01-acc.datapunt.amsterdam.nl')
+
+        response = self.update_metadata_onedate(self.path, 'BRK_zakelijk_recht')
+
+        self.assertNotEqual(response, None)
+        self.assertEqual(response.status_code, 200)
+
+    def testOneDateProd(self):
+        self.path = os.path.join(self.diva, 'brk')
+        self.set_hostname('ap01.datapunt.amsterdam.nl')
+
+        response = self.update_metadata_onedate(self.path, 'BRK_zakelijk_recht')
+
+        self.assertNotEqual(response, None)
+        self.assertEqual(response.status_code, 200)
