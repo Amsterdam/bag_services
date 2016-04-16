f6e7a78569f5f315a01641b099adfc98fa90ba6e
Kfir Breger <kfir@cubicalwave.com>
2016-04-16 22:11:46 +0200

    ATLAS-1515 Creating a query for exact match

--- atlas_import/datasets/bag/queries.py
+++ atlas_import/datasets/bag/queries.py
@@ -127,3 +127,13 @@ def public_area_Q(query):
             ],
         ),
     }
+
+def exact_postcode_house_number_Q(query):
+    """Create a query form an exact match on the address"""
+    return Q(
+        'bool',
+        must=[Q('keyword', postcode_huisnummer=query)],
+        should=[Q('keyword', postcode_toevoeging=query)],
+        minimum_should_match=1
+    )
+
