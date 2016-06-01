09bf84476fd4a57f5507be92a3b609d5ba28305c
Stephan Zaat <stephan@zosc.nl>
2016-06-01 22:46:47 +0200

    TG-23 - Devops - Move backend to Docker - testing import

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -14,8 +14,21 @@ trap 'dc kill ; dc rm -f' EXIT
 rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
+#dc build
+#dc run --rm importer_pg
+#dc run --rm importer_el1
+#sleep 10
+#dc run --rm importer_el2
+#dc run --rm importer_el3
+#dc run --rm db-backup
+#dc run --rm el-backup
+
 dc build
-dc run --rm importer
-dc run --rm db-backup
-dc run --rm el-backup
+dc run importer_pg
+dc run importer_el1
+sleep 10
+dc run importer_el2
+dc run importer_el3
+dc run db-backup
+dc run el-backup
 
