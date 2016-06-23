b00fab8e19f1bfa1d648cee3c92a445dea72f0bb
kpaska <kelvin.paska@gmail.com>
2016-06-23 15:44:30 +0200

    removed unused comments

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -14,24 +14,16 @@ trap 'dc kill ; dc rm -f' EXIT
 rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
-#dc build
-#dc run --rm importer_pg
-#dc up importer_bag1 importer_bag2 importer_bag3
-#dc up importer_brk1 importer_brk2 importer_brk3
-#dc run --rm db-backup
-#dc run --rm el-backup
-
-
 dc build
 # import new diva files in database
 dc run --rm importer
 # create the new elastic indexes
 dc up importer_el1 importer_el2 importer_el3
-# waint until all building is done
+# wait until all building is done
 docker wait jenkins_importer_el1_1 jenkins_importer_el2_1 jenkins_importer_el3_1
 
 # run the backup shizzle
 dc run --rm db-backup
 dc run --rm el-backup
 
-# Youare awsome and done! <3
+# You are awesome and done! <3
