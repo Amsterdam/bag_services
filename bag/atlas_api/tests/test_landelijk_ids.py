771f645920049fc005f81b7b7ddbcdbb2b7cc123
Stephan Preeker <stephan@preeker.net>
2016-08-17 16:46:54 +0200

    #TG-44 Landelijke_ids werken nu ook op nummeraanduiding

--- bag/atlas_api/tests/test_landelijk_ids.py
+++ bag/atlas_api/tests/test_landelijk_ids.py
@@ -7,6 +7,7 @@ class LandelijkIDTest(APITestCase):
 
     def setUp(self):
         self.vbo = bag_factories.VerblijfsobjectFactory.create()
+        self.na = bag_factories.NummeraanduidingFactory.create()
 
     def test_vbo_amsterdams_id(self):
         res = self.client.get('/bag/verblijfsobject/{}/'.format(self.vbo.id))
@@ -14,8 +15,19 @@ class LandelijkIDTest(APITestCase):
         self.assertEquals(self.vbo.id, res.data['sleutelverzendend'])
 
     def test_vbo_landelijk_id(self):
-        res = self.client.get('/bag/verblijfsobject/{}/'.format(self.vbo.landelijk_id))
+        res = self.client.get('/bag/verblijfsobject/{}/'.format(
+            self.vbo.landelijk_id))
         self.assertEquals(200, res.status_code)
         self.assertEquals(self.vbo.id, res.data['sleutelverzendend'])
 
+    def test_nummeraanduiding_amsterdams_id(self):
+        res = self.client.get('/bag/nummeraanduiding/{}/'.format(
+            self.na.id))
+        self.assertEquals(200, res.status_code)
+        self.assertEquals(self.na.id, res.data['sleutelverzendend'])
 
+    def test_nummeraanduiding_landelijk_id(self):
+        res = self.client.get('/bag/nummeraanduiding/{}/'.format(
+            self.na.landelijk_id))
+        self.assertEquals(200, res.status_code)
+        self.assertEquals(self.na.id, res.data['sleutelverzendend'])
