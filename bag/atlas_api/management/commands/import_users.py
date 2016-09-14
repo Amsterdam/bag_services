775b9efa510d3dc83861e06fadde7415fb66e4bd
Marten Hoekstra <marten@dolhuis.nl>
2016-09-14 16:47:11 +0200

    Backend TG-133 beheer van gebruikers aangepast aan de nieuwe rol van gebruikers

--- bag/atlas_api/management/commands/import_users.py
+++ bag/atlas_api/management/commands/import_users.py
@@ -1,6 +1,6 @@
 import csv
-
 import datetime
+
 from django.contrib.auth.models import User, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
@@ -10,6 +10,10 @@ from datasets.brk import models as brk_models
 
 
 class Command(BaseCommand):
+    view_sensitive_details_permission_brk = Permission.objects.get(
+        content_type=ContentType.objects.get_for_model(brk_models.KadastraalSubject),
+        codename='view_sensitive_details'
+    )
 
     def add_arguments(self, parser):
         parser.add_argument("csv_file")
@@ -17,24 +21,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         csv_file = options['csv_file']
 
-        view_sensitive_details_permission_brk = Permission.objects.get(
-            content_type=ContentType.objects.get_for_model(brk_models.KadastraalSubject),
-            codename='view_sensitive_details'
-        )
-
         with open(csv_file) as f:
             reader = csv.reader(f, delimiter=';')
             next(reader)  # skip header
 
             for row in reader:
                 username = row[0].lower()
-                password = row[1]
-                email = row[2]
-                joined = datetime.datetime.strptime(row[3], "%d-%m-%Y")
-                joined = joined.replace(tzinfo=UTC())
+                joined = datetime.datetime.strptime(row[1], "%d-%m-%Y").replace(tzinfo=UTC())
+                self.create_user(username, joined)
 
-                User.objects.filter(username=username).delete()
-                user = User.objects.create_user(username=username, password=password, email=email)
-                user.date_joined = joined
-                user.user_permissions.add(view_sensitive_details_permission_brk)
-                user.save()
+    def create_user(self, username, joined):
+        User.objects.filter(username=username).delete()
+        user = User.objects.create_user(username=username)
+        user.date_joined = joined
+        user.user_permissions.add(self.view_sensitive_details_permission_brk)
+        user.save()
