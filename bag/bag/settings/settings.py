0ba75ba5083df5b1f0e6e30056972ddf9536e091
Stephan Preeker <stephan@preeker.net>
2016-10-31 11:19:49 +0100

    make bag local settings work again

--- bag/bag/settings/settings.py
+++ bag/bag/settings/settings.py
@@ -219,6 +219,18 @@ SWAGGER_SETTINGS = {
     'doc_expansion': 'list',
 }
 
+OBJECTSTORE = {
+    'auth_version': '2.0',
+    'authurl': 'https://identity.stack.cloudvps.com/v2.0',
+    'user': os.getenv('OBJECTSTORE_USER', 'bag_brk'),
+    'key': os.getenv('OS_PASSWORD', 'insecure'),
+    'tenant_name': 'BGE000081_BAG',
+    'os_options': {
+        'tenant_id': '4f2f4b6342444c84b3580584587cfd18',
+        'region_name': 'NL',
+        'endpoint_type' : 'internalURL'}
+}
+
 JWT_AUTH = {
     'JWT_ENCODE_HANDLER':
         'rest_framework_jwt.utils.jwt_encode_handler',
