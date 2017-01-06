b7628db31b52e1ddfb14b53eb68520252829f765
Stephan Zaat <s.zaat@amsterdam.nl>
2017-01-06 13:54:18 +0100

    Fix remove docker images

--- .jenkins/remove-old-containers.sh
+++ .jenkins/remove-old-containers.sh
@@ -4,11 +4,11 @@ set -u
 set -e
 
 echo "clean up old dockers";
-for container_id in $(docker ps -a --filter status=exited -q);do docker rm $container_id;done
+for container_id in $(docker ps -aq --filter status=exited);do docker rm $container_id;done
 echo "clean up completed";
 
 echo "clean up old images";
-docker 2>/dev/null 1>&2 rmi `docker images -aq` || true
+docker 2>/dev/null 1>&2 rmi -f `docker images -aq` || true
 echo "clean up images completed";
 
 echo "clean up old volumes";
