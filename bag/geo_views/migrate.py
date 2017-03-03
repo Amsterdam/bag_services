af60b8d13a1c1641c67b9d1edffbce0584688b2e
Jeroen van 't Hert <jeroen@yottabyte.io>
2017-03-03 17:10:02 +0100

    Move to materialized views instead of tables.

--- bag/geo_views/migrate.py
+++ bag/geo_views/migrate.py
@@ -33,18 +33,36 @@ class ManageView(Operation):
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         schema_editor.execute(f'DROP VIEW IF EXISTS {self.view_name}')
+        schema_editor.execute(f'DROP TABLE IF EXISTS {self.view_name}_mat')
+        schema_editor.execute(f'DROP VIEW IF EXISTS {self.view_name}_mat')
+
         schema_editor.execute(f'CREATE VIEW {self.view_name} AS {self.sql}')
+        schema_editor.execute(f'CREATE MATERIALIZED VIEW {self.view_name}_mat '
+                              f'AS {self.sql}')
+        self._create_mat_geo_index(schema_editor, self.view_name)
 
     def database_backwards(self, app_label, schema_editor, from_state,
                            to_state):
         schema_editor.execute(f'DROP VIEW IF EXISTS {self.view_name}')
+        schema_editor.execute(f'DROP TABLE IF EXISTS {self.view_name}_mat')
+        schema_editor.execute(f'DROP VIEW IF EXISTS {self.view_name}_mat')
         previous = self.pop_previous_sql(app_label)
 
         if previous:
             schema_editor.execute(f'CREATE VIEW {self.view_name} AS {previous}')
+            schema_editor.execute(
+                f'CREATE MATERIALIZED VIEW {self.view_name}_mat AS {previous}')
 
     def state_forwards(self, app_label, state):
         self.push_history(app_label)
 
     def describe(self):
-        return "Create view {}".format(self.view_name)
+        return f"Create normal and materialized view {self.view_name}"
+
+    def _create_mat_geo_index(self, schema_editor, viewname, prefix='geo_'):
+        if prefix and self.view_name.startswith(prefix):
+            statement = f"""CREATE INDEX {viewname}_mat_idx
+                            ON {viewname}_mat
+                            USING  GIST ('geometrie')"""
+
+            schema_editor.execute(statement)
