f8528717610a387fff23252a82f1b75b49463914
kelvinpaska <k.paska@amsterdam.nl>
2017-07-19 09:23:29 +0200

    #536 - update to dc3.0

--- bag/docker-wait.sh
+++ bag/docker-wait.sh
@@ -4,14 +4,14 @@ set -u
 set -e
 
 # wait for elastic
-while ! nc -z ${ELASTICSEARCH_PORT_9200_TCP_ADDR} ${ELASTICSEARCH_PORT_9200_TCP_PORT}
+while ! nc -z elasticsearch 9200
 do
 	echo "Waiting for elastic..."
 	sleep 0.1
 done
 
 # wait for postgres
-while ! nc -z ${DATABASE_PORT_5432_TCP_ADDR} ${DATABASE_PORT_5432_TCP_PORT}
+while ! nc -z database 5432
 do
 	echo "Waiting for postgres..."
 	sleep 0.1
