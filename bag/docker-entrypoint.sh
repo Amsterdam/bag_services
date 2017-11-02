fa18a3a27affa1214e0de7446a40ad76625cd57d
Evert Lammerts <evertlammerts@users.noreply.github.com>
2017-11-02 11:09:23 +0100

    Don't migrate

--- bag/docker-entrypoint.sh
+++ bag/docker-entrypoint.sh
@@ -8,8 +8,8 @@ cd /app
 echo Performing system check
 python manage.py check
 
-echo Performing migrations
-yes yes | python manage.py migrate
+#echo Performing migrations
+#yes yes | python manage.py migrate
 
 echo Collecting static files
 yes yes | python manage.py collectstatic
