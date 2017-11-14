6bada240e93d53498fe7fb817c2292248564ef21
Bart Heupers <bart.heupers@gmail.com>
2017-11-14 15:05:41 +0100

    Fix JWKS verify only data

--- bag/bag/settings/settings.py
+++ bag/bag/settings/settings.py
@@ -210,13 +210,12 @@ JWKS_TEST_KEY = """
             {
                 "kty": "EC",
                 "key_ops": [
-                    "verify",
-                    "sign"
+                    "verify"
                 ],
                 "kid": "2aedafba-8170-4064-b704-ce92b7c89cc6",
                 "crv": "P-256",
                 "x": "6r8PYwqfZbq_QzoMA4tzJJsYUIIXdeyPA27qTgEJCDw=",
-                "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE=",
+                "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE="
             }
         ]
     }
