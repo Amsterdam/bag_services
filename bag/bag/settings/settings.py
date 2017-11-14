9a78ca39601d1334cf2552714bc1bc99378cea7d
Bart Heupers <bart.heupers@gmail.com>
2017-11-14 16:09:11 +0100

    Fix JWKS. Set complete test sign and verify JWKS in settings

--- bag/bag/settings/settings.py
+++ bag/bag/settings/settings.py
@@ -203,22 +203,24 @@ REST_FRAMEWORK = dict(
 )
 
 # The following JWKS data was obtained in the authz project :  jwkgen -create -alg ES256
-# This is a test public key def. The private key is defined mixins.py for testing.
+# This is a test public key def. The private key is add for testing.
 JWKS_TEST_KEY = """
-    {
-        "keys": [
             {
-                "kty": "EC",
-                "key_ops": [
-                    "verify"
-                ],
-                "kid": "2aedafba-8170-4064-b704-ce92b7c89cc6",
-                "crv": "P-256",
-                "x": "6r8PYwqfZbq_QzoMA4tzJJsYUIIXdeyPA27qTgEJCDw=",
-                "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE="
+                "keys": [
+                    {
+                        "kty": "EC",
+                        "key_ops": [
+                            "verify",
+                            "sign"
+                        ],
+                        "kid": "2aedafba-8170-4064-b704-ce92b7c89cc6",
+                        "crv": "P-256",
+                        "x": "6r8PYwqfZbq_QzoMA4tzJJsYUIIXdeyPA27qTgEJCDw=",
+                        "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE=",
+                        "d": "N1vu0UQUp0vLfaNeM0EDbl4quvvL6m_ltjoAXXzkI3U="
+                    }
+                ]
             }
-        ]
-    }
 """
 
 DATAPUNT_AUTHZ = {
--- bag/datasets/generic/tests/authorization.py
+++ bag/datasets/generic/tests/authorization.py
@@ -1,7 +1,6 @@
 import time
 import jwt
 
-from django.conf import settings
 from authorization_django.config import settings as middleware_settings
 
 import authorization_levels
@@ -39,28 +38,8 @@ class AuthorizationSetup(object):
             HTTP_AUTHORIZATION='Bearer {}'.format(self.token_employee_plus))
 
         """
-        # NEW STYLE AUTH
         # VERY NEW STYLE AUTH
-        # The following JWKS data was obtained in the authz project :  jwkgen -create -alg ES256
-        # This is a test private/public key pair.
-        settings.DATAPUNT_AUTHZ['JWKS'] = """
-            {
-                "keys": [
-                    {
-                        "kty": "EC",
-                        "key_ops": [
-                            "verify",
-                            "sign"
-                        ],
-                        "kid": "2aedafba-8170-4064-b704-ce92b7c89cc6",
-                        "crv": "P-256",
-                        "x": "6r8PYwqfZbq_QzoMA4tzJJsYUIIXdeyPA27qTgEJCDw=",
-                        "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE=",
-                        "d": "N1vu0UQUp0vLfaNeM0EDbl4quvvL6m_ltjoAXXzkI3U="
-                    }
-                ]
-            }
-        """
+        # Use JWKS defined in settings
         jwks = middleware_settings()['JWKS'].signers
         assert len(jwks) > 0
         (kid, key), = jwks.items()
