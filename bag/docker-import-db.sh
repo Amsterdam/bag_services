fc1568039481327475bb35c3528c1771f29d8632
Stephan Preeker <stephan@preeker.net>
2017-12-07 11:15:44 +0100

    tg-#533 bag naar elk5

---
+++ bag/docker-import-db.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -u   # crash on missing environment variables
+set -e   # stop on any error
+set -x   # log every command.
+
+source docker-wait.sh
+
+# download csv
+python objectstore/objectstore.py
+
+# load dat in database
+python manage.py migrate
+python manage.py run_import --no-index
+
+# clear elasticindices
+python manage.py elastic_indices --delete
