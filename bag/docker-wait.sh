798ad233959c47a741143888437e3aac58d13360
Stephan Preeker <stephan@preeker.net>
2017-12-13 12:01:00 +0100

    BAC-622 brk and bag import seperate

--- bag/docker-wait.sh
+++ bag/docker-wait.sh
@@ -4,14 +4,14 @@ set -u
 set -e
 
 # wait for elastic
-while ! nc -z ${ELASTIC_HOST_OVERRIDE} ${ELASTIC_PORT_OVERRIDE}
+while ! nc -z elasticsearch 9200
 do
 	echo "Waiting for elastic..."
 	sleep 2
 done
 
 # wait for postgres
-while ! nc -z ${DATABASE_HOST_OVERRIDE} ${DATABASE_PORT_OVERRIDE}
+while ! nc -z database 5432
 do
 	echo "Waiting for postgres..."
 	sleep 2
