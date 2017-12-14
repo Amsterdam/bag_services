52cc28c77d636950c8fa163eab30a97d47b2264d
Stephan Preeker <stephan@preeker.net>
2017-12-14 20:06:43 +0100

    just import 1 process..

--- bag/docker-import-db.sh
+++ bag/docker-import-db.sh
@@ -11,7 +11,4 @@ python objectstore/objectstore.py
 
 # load dat in database
 python manage.py migrate
-python manage.py run_import bag --no-index &  \
-python manage.py run_import brk --no-index
-
-python manage.py run_import wkpb --no-index
+python manage.py run_import --no-index
