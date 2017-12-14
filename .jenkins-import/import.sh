b4a643082d8739236f33afe2ed73207d1b382a8d
Stephan Preeker <stephan@preeker.net>
2017-12-14 12:03:30 +0100

    BAC-622 import shizzle jenkins

--- .jenkins-import/import.sh
+++ .jenkins-import/import.sh
@@ -15,8 +15,13 @@ rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
 echo "Building dockers"
+dc down
 dc pull
 dc build
+
+dc up -d database
+dc up -d elasticsearch
+dc run importer ./docker-wait.sh
 
 echo "import new diva files into database"
 dc run --rm importer
@@ -24,8 +29,9 @@ dc run --rm importer
 echo "Starting Elastic importers"
 dc run --rm importer ./docker-index-es.sh
 
+
 echo "Running backups"
-dc run --rm db-backup
+dc exec database ./backup-bagdb.sh
 dc exec elasticsearch backup-indices.sh bag bag,brk,nummeraanduiding
 
 echo "Done"
