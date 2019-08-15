338dbdf6bba8e9a30926a59df8d96accd4975a1d
Bart Heupers <bart.heupers@gmail.com>
2019-08-15 14:45:55 +0200

    Use old wkpb bron documents as long as wkpb metadata is still old

--- bag/datasets/wkpb/models.py
+++ bag/datasets/wkpb/models.py
@@ -98,7 +98,9 @@ class Brondocument(models.Model):
 
     @property
     def url(self):
-        return f'{settings.DATAPUNT_API_URL}wkpb/brondocument/{self.id}/?as_pdf'
+        # return f'{settings.DATAPUNT_API_URL}wkpb/brondocument/{self.id}/?as_pdf'
+        base_url = 'http://diva.intranet.amsterdam.nl/Brondocumenten/Wkpb'
+        return '%s/%s' % (base_url, self.documentnaam)
 
     class Meta:
         verbose_name = "Brondocument"
