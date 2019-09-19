03bcda4a36b6dce7177ae4b49093d3d168169ec6
obdeijn <arnold.obdeijn@gmail.com>
2019-09-19 16:34:57 +0200

    Use new version of authorization-django - use authorization-django from master branch - adapt test suite and settings to new version

--- bag/datasets/generic/tests/authorization.py
+++ bag/datasets/generic/tests/authorization.py
@@ -1,7 +1,7 @@
 import time
-import jwt
 
-from authorization_django.config import settings as middleware_settings
+from jwcrypto.jwt import JWT
+from authorization_django.jwks import get_keyset
 
 from bag import authorization_levels
 
@@ -38,38 +38,81 @@ class AuthorizationSetup(object):
             HTTP_AUTHORIZATION='Bearer {}'.format(self.token_employee_plus))
 
         """
-        # VERY NEW STYLE AUTH
-        # Use JWKS defined in settings
-        jwks = middleware_settings()['JWKS'].signers
-        assert len(jwks) > 0
-        (kid, key), = jwks.items()
+        jwks = get_keyset()
+        assert len(jwks['keys']) > 0
 
+        key = next(iter(jwks['keys']))
         now = int(time.time())
 
-        token_default = jwt.encode({
-            'scopes': [],
-            'iat': now, 'exp': now + 600}, key.key, algorithm=key.alg, headers={'kid': kid})
-        token_employee_plus = jwt.encode({
-            'scopes': [
-                authorization_levels.SCOPE_BRK_RSN, authorization_levels.SCOPE_BRK_RS,
-                authorization_levels.SCOPE_BRK_RO, authorization_levels.SCOPE_WKPB_RBDU],
-            'iat': now, 'exp': now + 600}, key.key, algorithm=key.alg, headers={'kid': kid})
-        token_scope_brk_rs = jwt.encode({
-            'scopes': [authorization_levels.SCOPE_BRK_RS],
-            'iat': now, 'exp': now + 600}, key.key, algorithm=key.alg, headers={'kid': kid})
-        token_scope_brk_rsn = jwt.encode({
-            'scopes': [authorization_levels.SCOPE_BRK_RSN, authorization_levels.SCOPE_BRK_RS],
-            'iat': now, 'exp': now + 600}, key.key, algorithm=key.alg, headers={'kid': kid})
-        token_scope_brk_ro = jwt.encode({
-            'scopes': [authorization_levels.SCOPE_BRK_RO],
-            'iat': now, 'exp': now + 600}, key.key, algorithm=key.alg, headers={'kid': kid})
-        token_scope_wkpd_rdbu = jwt.encode({
-            'scopes': [authorization_levels.SCOPE_WKPB_RBDU],
-            'iat': now, 'exp': now + 600}, key.key, algorithm=key.alg, headers={'kid': kid})
+        header = {
+            'alg': 'ES256',  # algorithm of the test key
+            'kid': key.key_id
+        }
 
-        self.token_default = str(token_default, 'utf-8')
-        self.token_employee_plus = str(token_employee_plus, 'utf-8')
-        self.token_scope_brk_rs = str(token_scope_brk_rs, 'utf-8')
-        self.token_scope_brk_rsn = str(token_scope_brk_rsn, 'utf-8')
-        self.token_scope_brk_ro = str(token_scope_brk_ro, 'utf-8')
-        self.token_scope_wkpd_rdbu = str(token_scope_wkpd_rdbu, 'utf-8')
+        token_default = JWT(
+            header=header,
+            claims={
+                'iat': now, 'exp': now + 600, 'scopes': []
+            }
+        )
+        token_employee_plus = JWT(
+            header=header,
+            claims={
+                'iat': now, 'exp': now + 600,
+                'scopes': [
+                    authorization_levels.SCOPE_BRK_RSN,
+                    authorization_levels.SCOPE_BRK_RS,
+                    authorization_levels.SCOPE_BRK_RO,
+                    authorization_levels.SCOPE_WKPB_RBDU
+                ]
+            }
+        )
+        token_scope_brk_rs = JWT(
+            header=header,
+            claims={
+                'iat': now, 'exp': now + 600,
+                'scopes': [authorization_levels.SCOPE_BRK_RS]
+            }
+        )
+        token_scope_brk_rsn = JWT(
+            header=header,
+            claims={
+                'iat': now, 'exp': now + 600,
+                'scopes': [
+                    authorization_levels.SCOPE_BRK_RSN,
+                    authorization_levels.SCOPE_BRK_RS
+                ]
+            }
+        )
+        token_scope_brk_ro = JWT(
+            header=header,
+            claims={
+                'iat': now, 'exp': now + 600,
+                'scopes': [authorization_levels.SCOPE_BRK_RO]
+            }
+        )
+        token_scope_wkpd_rdbu = JWT(
+            header=header,
+            claims={
+                'iat': now, 'exp': now + 600,
+                'scopes': [authorization_levels.SCOPE_WKPB_RBDU]
+            }
+        )
+
+        token_default.make_signed_token(key)
+        self.token_default = token_default.serialize()
+
+        token_employee_plus.make_signed_token(key)
+        self.token_employee_plus = token_employee_plus.serialize()
+
+        token_scope_brk_rs.make_signed_token(key)
+        self.token_scope_brk_rs = token_scope_brk_rs.serialize()
+
+        token_scope_brk_rsn.make_signed_token(key)
+        self.token_scope_brk_rsn = token_scope_brk_rsn.serialize()
+
+        token_scope_brk_ro.make_signed_token(key)
+        self.token_scope_brk_ro = token_scope_brk_ro.serialize()
+
+        token_scope_wkpd_rdbu.make_signed_token(key)
+        self.token_scope_wkpd_rdbu = token_scope_wkpd_rdbu.serialize()
