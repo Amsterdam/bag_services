9d4c80ecf8acbf05e77a361cfc93be1c5f496dd5
Bart Heupers <bart.heupers@gmail.com>
2020-03-11 15:04:09 +0100

    Fix sentry error https://sentry.data.amsterdam.nl/sentry/datapunt-apis-prod/issues/20378/?query=is%3Aunresolved
    AttributeError
    'NoneType' object has no attribute 'get'

--- bag/datasets/generic/renderers.py
+++ bag/datasets/generic/renderers.py
@@ -5,6 +5,6 @@ class PaginatedCSVRenderer(CSVRenderer):
     results_field = 'results'
 
     def render(self, data, *args, **kwargs):
-        if not isinstance(data, list):
+        if data and not isinstance(data, list):
             data = data.get(self.results_field, [])
         return super(PaginatedCSVRenderer, self).render(data, *args, **kwargs)
