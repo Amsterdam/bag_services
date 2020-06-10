ea9beac8e69e92c8162575fa19a1398d9a0c8978
Bart Heupers <bart.heupers@gmail.com>
2020-06-10 14:45:34 +0200

    Revert "DI-495 - Make typeahead search for nummeraanduiding better. (#160)"
    This reverts commit 38a31073f102a71383ab08e8948c1c6059c93455.

--- bag/search/queries.py
+++ bag/search/queries.py
@@ -11,16 +11,13 @@ def create_search_query(
         query: dict,
         indexes: List[str],
         sort_fields: List[str] = None,
-        size: int = 15,
-        search_type = None,
+        size: int = 15
 ) -> Search:
     """
     :param query: an elastic search query
     :param indexes: list of indexes to use
     :param sort_fields: an optional list of fields to use for server-side sorting
     :param size: an optional limit on size of the result set
-    :param search_type: search_type to be passed. For example dfs_query_then_fetch. See :
-      https://www.elastic.co/guide/en/elasticsearch/reference/master/search-request-body.html#dfs-query-then-fetch
     """
     assert indexes
 
@@ -29,8 +26,6 @@ def create_search_query(
         .index(*indexes)
         .query(query)
     )
-    if search_type:
-        search = search.params(search_type=search_type)
     if sort_fields:
         search = search.sort(*sort_fields)
 
