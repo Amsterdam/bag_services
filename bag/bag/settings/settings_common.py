284bd483138b383f3b98cc87a7c73d512d35a997
Chris van Riel <c.van.riel@amsterdam.nl>
2021-01-19 11:16:54 +0100

    [DS-566] moved base.html to existing template folder

--- bag/bag/settings/settings_common.py
+++ bag/bag/settings/settings_common.py
@@ -87,7 +87,7 @@ if DEBUG:
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'datapunt_api/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
