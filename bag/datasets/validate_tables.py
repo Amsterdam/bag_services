68c8404ede502ff533287ffd02edddedb508a099
Jasper Jager <mail@jasperjager.com>
2021-06-09 13:26:54 +0200

    Fix bouwblokken margin

--- bag/datasets/validate_tables.py
+++ bag/datasets/validate_tables.py
@@ -10,7 +10,7 @@ LOG = logging.getLogger(__name__)
 
 TABLE_TARGETS = [
     # counts override, from 22-08-2019
-    (8888, 0, "bag_bouwblok"),
+    (9384, 0, "bag_bouwblok"),
     (481, 0, "bag_buurt"),
     (110, 11, "bag_buurtcombinatie"),  # Temporary allow a bigger margin, until Weesp is added in production
     (22, 6, "bag_gebiedsgerichtwerken"),
