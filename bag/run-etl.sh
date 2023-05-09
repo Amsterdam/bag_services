b18d761287eb7232c9770dcd92d0c0d6ad6a619d
DonQueso89 <k.van.ekeren@amsterdam.nl>
2023-05-09 13:22:38 +0200

    etl shellscript

---
+++ bag/run-etl.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Script for running the complete ETL process using data from the reference database.
+# The data resulting from this process is served through the REST API
+# included in this repository.  
+#
+# The process entails:
+#   - loading data from the reference database into a separate schema (e.g. bag_v11)
+#   - using the loaded data to create/update the indices in Elasticsearch
+#
+# TODO: Note that currently this will incur downtime since we are truncating and writing
+# directly to the target databases.
+
+FILENAME=$(realpath $0) 
+CURDIR=${FILENAME%/*} 
+
+echo 'Truncating legacy schema tables'
+$CURDIR/ref-db/cli.py --delete
+echo 'Running data transfer from refdb to legacy schema'
+$CURDIR/ref-db/cli.py --execute
+
+echo 'Running denormalization task'
+$CURDIR/manage.py run_import --task-filter denormalize_vbo_standplaats_ligplaats
+
+echo 'Deleting Elasticsearch indices'
+$CURDIR/manage.py elastic_indices --delete
+echo 'Starting Elasticsearch indexing'
+$CURDIR/manage.py elastic_indices --build
+
+echo 'ETL process successful'
