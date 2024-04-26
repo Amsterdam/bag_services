d75d62b898db0a40c81f55e87684535bebd5cb30
jjmurre <jan.murre@gmail.com>
2024-04-26 19:39:49 +0200

    Tweak the old es indexing script

--- bag/docker-index-es.sh
+++ bag/docker-index-es.sh
@@ -4,35 +4,16 @@ set -u   # crash on missing environment variables
 set -e   # stop on any error
 set -x   # log every command.
 
-trap "kill 0" EXIT
-
-source docker-wait.sh
-
 # clear elasticindices and creates empty ones
+# It seems that this step is essential to get the correct mapping
+# for the indexes!
 python manage.py elastic_indices --delete
 
 python manage.py elastic_indices gebieden pand --build
 
-python manage.py elastic_indices bag brk --partial=1/3 --build &
-python manage.py elastic_indices bag brk --partial=2/3 --build &
-python manage.py elastic_indices bag brk --partial=3/3 --build &
-
-
-FAIL=0
-
-for job in `jobs -p`
-do
-	echo $job
-	wait $job || let "FAIL+=1"
-done
-
-echo $FAIL
-
-if [ "$FAIL" == "0" ];
-then
-    echo "YAY!"
-else
-    echo "FAIL! ($FAIL)"
-    echo 'Elastic Import Error. 1 or more workers failed'
-    exit 1
-fi
+python manage.py elastic_indices bag --partial=1/3 --build 
+python manage.py elastic_indices bag --partial=2/3 --build 
+python manage.py elastic_indices bag --partial=3/3 --build 
+python manage.py elastic_indices brk --partial=1/3 --build 
+python manage.py elastic_indices brk --partial=2/3 --build 
+python manage.py elastic_indices brk --partial=3/3 --build 
