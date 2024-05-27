53a6ebb7a6b2a89464798411f78106fd59e948f6
lennard <lennard@stam.cc>
2024-05-27 12:03:24 +0200

    include delete commando

--- bag/docker-index-es.sh
+++ bag/docker-index-es.sh
@@ -10,11 +10,21 @@ retries=3
 re='^[0-9]+$'
 n=0
 
+# loop over netcat to see open psql & eslastic established connection(s)
+# source docker-wait.sh
+
 function usage() {
     echo "ERROR: Missing or invalid arguments!"
     echo "Usage: $0 [dataset] [parts]"
     exit 0
 }
+
+function rm_index() {
+    echo "Deleting elastic indexes!"
+    python manage.py elastic_indices --delete
+    exit 0
+}
+
 
 function run_index() {
   for num in $(seq 1 $parts); do
@@ -29,7 +39,9 @@ function run_index() {
 
 
 # Check if the right number of arguments were passed
-if [[ "$#" -eq 2 ]]; then
+if [[ $1 == "delete" ]]; then
+    rm_index
+elif [[ "$#" -eq 2 ]]; then
   if [[ $2 == ?(-)+([0-9]) ]]; then
     run_index
   else
@@ -38,5 +50,3 @@ if [[ "$#" -eq 2 ]]; then
 else
   usage
 fi
-
-exit 0
